8562c4e0f46d8efbe733b363d93c9250d8e2e28b
Michael <mikemason1987@gmail.com>
2021-04-29 20:54:45 -0400

    Began working the front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
 import './components/css/App.css';
+import Heading from './components/Header.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About'
+import Main from './components/Main'
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Heading />
       </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/main">Main</Link>
+              </li>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />  
+            </Route> 
+            <Route path ="/main">
+              <Main />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <p>snakes and shit are all around and things like that</p>
+    </div>
+  )
+}
+
+export default About
+
